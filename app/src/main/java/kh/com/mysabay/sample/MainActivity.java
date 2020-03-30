356f8d71350c53d6455609ea0646c160b38ea2dd
Tan Phirum <phirumtan@gmail.com>
2020-03-31 00:36:12 +0700

    add new configuration for sdk

--- app/src/main/java/kh/com/mysabay/sample/MainActivity.java
+++ app/src/main/java/kh/com/mysabay/sample/MainActivity.java
@@ -7,10 +7,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import kh.com.mysabay.sample.databinding.ActivityMainBinding;
-import kh.com.mysabay.sample.utils.MessageUtil;
 import kh.com.mysabay.sdk.MySabaySDK;
+import kh.com.mysabay.sdk.SdkConfiguration;
 import kh.com.mysabay.sdk.callback.LoginListener;
 import kh.com.mysabay.sdk.callback.PaymentListener;
+import kh.com.mysabay.sdk.utils.MessageUtil;
+import kh.com.mysabay.sdk.utils.SdkTheme;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final SdkConfiguration configuration = new SdkConfiguration.Builder(BuildConfig.APPLICATION_ID,
+                "9c85c50a4362f687cd4507771ba81db5cf50eaa0b3008f4f943f77ba3ac6386b",
+                "",
+                "")
+                .setSdkTheme(SdkTheme.Dark)
+                .setToUseSandBox(true).build();
+        MySabaySDK.Impl.setDefaultInstanceConfiguration(configuration);
+
         mViewBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         mViewBinding.viewPb.setVisibility(View.GONE);
         findViewById(R.id.show_login_screen).setOnClickListener(v -> {
@@ -57,8 +68,8 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                     @Override
-                    public void purchaseAIPSuccess(Object dataAIP) {
-                        MessageUtil.displayToast(v.getContext(), "dataAIP = " + dataAIP);
+                    public void purchaseIAPSuccess(Object dataIAP) {
+                        MessageUtil.displayToast(v.getContext(), "dataAIP = " + dataIAP);
                     }
 
                     @Override
